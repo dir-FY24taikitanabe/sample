ec60e94bf866d01a405452d6e0904296a04493e8
dir-FY24taikitanabe <TaikiTanabe()@Dirbato-1316x>
2024-04-08 11:59:38 +0900

    コンパイラの準拠レベル10にしただけ

---
+++ sample/src/sample/ArraySample.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class ArraySample {
+
+    public static void main(String[] args) {
+        var numbers = new int[5];
+        
+        
+        for(var =i;i<numbers.length;i++) {
+            numbers[i]=i*2;
+        }
+        
+        for(vari=0;i<numbers.length;i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+}
